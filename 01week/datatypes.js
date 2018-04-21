b4ca047c1b6ebc3c829013ebdf99be741d0c4589
Lesley Sablatura <lesleysab210@gmail.com>
2018-04-21 09:58:16 -0500

    updates from code review

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,42 +1,27 @@
-// Write a JavaScript function to display the current day and time.
-
+/// 1. Write a JavaScript function to display the current day and time.
 // getDate()
 // getTime()
 // new Date()
-
 function displayDT() {
   const now = new Date();
   return now;
 }
 displayDT();
-//
-
-
-
-// Write a JavaScript function to convert a number to a string.
-
+// 2. Write a JavaScript function to convert a number to a string.
 // toString()
-
 function numToStr(x) {
   return x.toString();
 }
 numToStr(20);
-
-
-// Write a JavaScript function to convert a string to the number.
-//
+// 3. Write a JavaScript function to convert a string to the number
 // Number()
 // or intparse
-
 function strToNum(str) {
   const num = parseInt(str, 10);
   return num;
 }
 strToNum("42px")
-
-
-// Write a JavaScript function that takes in different datatypes and prints out whether they are a:
-//
+// 4. Write a JavaScript function that takes in different datatypes and prints out whether they are a:
 // typeof ()
 function dataTypes(value) {
   const x = null;
@@ -48,49 +33,35 @@ dataTypes(typeof x); //undefined
 dataTypes(typeof 12); //number
 dataTypes(typeof NaN); //NaN
 dataTypes(typeof null); //null
-
-
-// Write a JavaScript program that adds 2 numbers together.
-
+// 5. Write a JavaScript program that adds 2 numbers together.
 function sumTwoNumbers(num1, num2) {
   return num1 + num2
 }
 sumTwoNumbers(4, 6);
-
-// Write a JavaScript function that runs only when 2 things are true.
-//
+// 6. Write a JavaScript function that runs only when 2 things are true.
 // &&
-
 function twoTruths(num1, num2) {
   if (num1 <= 5 && num2 >= 10)
-    return ("true");
+    return (num1 + " is less than or equal to 5", num2 + " is greater than or equal to 10");
   else
-    return ('false');
+    return (num1 + " is greater than or equal to 5", num2 + " is less than or equal to 10");
 }
-twoTruths(6, 11);
-
-// Write a JavaScript program that runs when 1 of 2 things are true.
-//
+console.log(twoTruths(6, 11));
+// 7. Write a JavaScript program that runs when 1 of 2 things are true
 // ||
-
 function orTrue(num1, num2) {
   if (num1 <= 5 || num2 >= 10)
-    return ("true");
+    return (num1 + " is less than or equal to 5", num2 + " is greater than or equal to 10");
   else
-    return ('false');
+    return (num1 + " is greater than or equal to 5", num2 + " is less than or equal to 10");
 }
-orTrue(6, 9);
-
-
-// Write a JavaScript program that runs when both things are not true
-//
+console.log(orTrue(9, 10));
+// 8. Write a JavaScript program that runs when both things are not true
 // !&&!
-
-
 function noTrue(num1, num2) {
   if (!num1 == 5 || !num2 == 10)
-    return ("true");
+    return (num1 + " is not less than or equal to 5", num2 + " is not greater than or equal to 10");
   else
-    return ('false');
+    return (num1 + " is not greater than or equal to 5", num2 + " is not less than or equal to 10");
 }
-noTrue(5, 10);
+console.log(noTrue(5, 10));
