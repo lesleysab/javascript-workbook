cf89311b2fbe9f604306730ccb692d6b354b1f1f
Lesley Sablatura <lesleysab210@gmail.com>
2018-04-26 19:48:03 -0500

    updated

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -8,16 +8,104 @@ const rl = readline.createInterface({
 });
 
 
+
 function rockPaperScissors(hand1, hand2) {
 
   // Write code here
-
+  // version 1
+  if (hand1 === "rock" && hand2 === "scissors") {
+    // If User1 input is 'rock' and User2 input is 'scissor', User1 wins.
+    return "Hand one wins!";
+  } else
+  if (hand1 === "paper" && hand2 === "rock") {
+    // If User1 input is 'paper' and User2 input is 'rock', User1 wins.
+    return "Hand one wins!!";
+  } else if (hand1 === "scissors" && hand2 === "paper") {
+    // If User1 input is 'scissors' and User2 input is 'paper', User1 wins.
+    return "Hand one wins!";
+  } else if (hand1 === "scissors" && hand2 === "rock") {
+    // If User1 input is 'scissors' and User2 input is 'rock', User2 wins.
+    return "Hand two wins!";
+  } else if (hand1 === "paper" && hand2 === "scissors") {
+    // If User1 input is 'paper' and User2 input is 'scissors', User2 wins.
+    return "Hand two wins!";
+  } else if (hand1 === "rock" && hand2 === "paper") {
+    // If User1 input is 'rock' and User2 input is 'paper', User2 wins.
+    return "Hand two wins!";
+  } else if (hand1 === hand2) {
+    return "It's a tie!";
+  }
 }
+
+
+
+// version 2
+// if (hand1 === "rock" && hand2 === "scissors") {
+//   // If User1 input is 'rock' and User2 input is 'scissor', User1 wins.
+//   return "Hand one wins!";
+// } else if (hand1 === "paper" && hand2 === "rock") {
+//   // If User1 input is 'paper' and User2 input is 'rock', User1 wins.
+//   return "Hand one wins!";
+// } else if (hand1 === "scissors" && hand2 === "paper") {
+//
+//   // If User1 input is 'scissors' and User2 input is 'paper', User1 wins.
+//   return "Hand one wins!";
+// } else if (hand1 === "scissors" && hand2 === "rock") {
+//   // If User1 input is 'scissors' and User2 input is 'rock', User2 wins.
+//   return "Hand two wins!";
+// } else if (hand1 === "paper" && hand2 === "scissors") {
+//   // If User1 input is 'paper' and User2 input is 'scissors', User2 wins.
+//   return "Hand two wins!";
+// } else if (hand1 === "rock" && hand2 === "paper") {
+//
+//   // If User1 input is 'rock' and User2 input is 'paper', User2 wins.
+//   return "Hand two wins!";
+// } else if (hand1 === hand2) {
+//   return "It's a tie!";
+// }
+// version 3
+//   if (hand1 === "rock") {
+//     if (hand2 === "scissors") {
+//       //rock wins - hand1
+//       return "Hand one wins!";
+//     } else {
+//       //paper wins - hand2
+//       return "Hand two wins!";
+//     }
+//   }
+//
+//
+//   if (hand1 === "paper") {
+//     if (hand2 === "rock") {
+//       //paper wins-hand1
+//       return "Hand one wins!";
+//     } else {
+//       //rock wins - hand2
+//       return "Hand two wins!";
+//     }
+//   }
+//
+//
+//   if (hand1 === "scissors") {
+//     if (hand2 === "rock") {
+//       //rock wins - hand2
+//       return "Hand two wins!";
+//     } else {
+//       //scissors wins - hand1
+//       return "Hand one wins!";
+//     }
+//   }
+// }
+
+
+
+
+
 
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
     rl.question('hand2: ', (answer2) => {
-      console.log( rockPaperScissors(answer1, answer2) );
+      console.log(rockPaperScissors(answer1, answer2));
       getPrompt();
     });
   });
@@ -39,13 +127,13 @@ if (typeof describe === 'function') {
       assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
     });
     it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
-      assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
+      assert.equal(rockPaperScissors('rOcK', 'paper'), "Hand two wins!");
       assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
+      assert.equal(rockPaperScissors('rock', 'sCiSsOrs'), "Hand one wins!");
     });
   });
 } else {
-
+  //
   getPrompt();
 
 }
