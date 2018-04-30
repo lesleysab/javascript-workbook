cba79de47d26b5a97689dd7a83d4522dfeb7f284
Lesley Sablatura <lesleysab210@gmail.com>
2018-04-30 16:35:52 -0500

    Inital commit

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -7,17 +7,73 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-
+// write a function that takes in 1 string (str) global
 function pigLatin(word) {
+  //define vowels
+  const vowels = ["a", "e", "i", "o", "u"];
 
-  // Your code here
+  let splitWord = word.split('');
 
+
+
+  //does the vowels array include one of those letters in the first letter of the string?
+  if (vowels.includes(word.charAt(0))) {
+    // if the string starts with a vowel add 'ay' to the end of the string
+    return word.toLowerCase().replace(/ /g, '') + 'yay';
+  } else {
+    // store the consonants before the vowel in a string
+    // we need a function that adds the letters before the vowel to the end of the string
+    for (let index = 0; index < word.length; index++) {
+      if (!vowels.includes(word[index])) {
+        splitWord.push(splitWord.shift());
+      } else {
+        // it adds them to the end of the string
+        // and then adds “ay” to the end of the string
+        splitWord.push('ay');
+        return splitWord.join('').toLowerCase().replace(/ /g, '');
+
+      }
+    }
+  }
 }
+
+// example:
+// trace => ace-tr-ay
+// variables:
+// originalString: global   = trace
+// stringContainsConsonants: local = tr
+// newAddsConsonants: local = ace+tr
+// ayString: local acetr + ay
+// when you find the first vowel and it’s not the first letter, then do these things….
+// store the consonants before the vowel in a string
+// we need a function that adds the letters before the vowel to the end of the string
+// .append()
+// .shift
+// .push
+//
+// split the string
+// .trim
+//
+// if check to see if it’s a vowel
+// .split
+//
+
+// for  it will break on first character when it’s a vowel
+// first to last
+// .join
+// .append()
+//
+// else if the string starts with a vowel
+// then do this : we need a function that adds ay to the end of the string
+// Your code here
+
+
+// console.log(pigLatin("proud"));
 
 
 function getPrompt() {
   rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
+    console.log(pigLatin(answer));
     getPrompt();
   });
 }
