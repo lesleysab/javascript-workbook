853ddd58d34fc8451fbdee680c27c6407e7c92f9
Lesley Sablatura <lesleysab210@gmail.com>
2018-05-04 10:14:18 -0500

    Initial Commit Towers of Hanoi

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -21,6 +21,12 @@ function printStacks() {
 
 function movePiece() {
   // Your code here
+  //there are 3 stacks. abc
+  //4 discs 1234 start on stack a.
+  //click on smallest disc to move to another stack. b or c
+  //large disc cannot be placed on top of a smaller disc.
+  //win = all 4 discs are stacked in order 1234 on stack c.
+
 
 }
 
@@ -36,6 +42,13 @@ function checkForWin() {
 
 function towersOfHanoi(startStack, endStack) {
   // Your code here
+  // Move disk from stack  A  to stack  c
+  // Move disk from stack  a  to stack  b
+  // Move disk from   stack c to stack  b
+  // Move disk from stack  a  to stack  c
+  // Move disk from stack  b  to stack  a
+  // Move disk from stack  b  to stack  c
+  // Move disk from stack  a  to stack  c
 
 }
 
@@ -56,7 +69,11 @@ if (typeof describe === 'function') {
   describe('#towersOfHanoi()', () => {
     it('should be able to move a block', () => {
       towersOfHanoi('a', 'b');
-      assert.deepEqual(stacks, { a: [4, 3, 2], b: [1], c: [] });
+      assert.deepEqual(stacks, {
+        a: [4, 3, 2],
+        b: [1],
+        c: []
+      });
     });
   });
 
@@ -80,9 +97,17 @@ if (typeof describe === 'function') {
   });
   describe('#checkForWin()', () => {
     it('should detect a win', () => {
-      stacks = { a: [], b: [4, 3, 2, 1], c: [] };
+      stacks = {
+        a: [],
+        b: [4, 3, 2, 1],
+        c: []
+      };
       assert.equal(checkForWin(), true);
-      stacks = { a: [1], b: [4, 3, 2], c: [] };
+      stacks = {
+        a: [1],
+        b: [4, 3, 2],
+        c: []
+      };
       assert.equal(checkForWin(), false);
     });
   });
