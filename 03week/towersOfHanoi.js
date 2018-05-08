7271b125cc33fbf8f1b18843c2f885a2b722ce33
Lesley Sablatura <lesleysab210@gmail.com>
2018-05-07 20:43:34 -0500

    Updates

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -13,24 +13,16 @@ let stacks = {
   c: []
 };
 
-function printStacks() {
+const printStacks = () => {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
-
-  let stack1 = stacks.a
-  let stack2 = stacks.b
-  let stack3 = stacks.c
-  let disc1 = "x"
-
-
-
+const movePiece = (origin, originDisc, destination) => {
+  origin.pop();
+  destination.push(originDisc);
 }
-
-
 // Your code here
 //I will use if/else statements to move discs on stacks.
 //there are 3 stacks. abc
@@ -39,50 +31,64 @@ function movePiece() {
 //if startStack is b, else end Stack can be stack a or c.
 // if startStack is c, then endStack can be a or b.
 //large disc cannot be placed on top of a smaller disc. run isLegal
-//myArray.push("this"); // push a string on the stack
+//myArray.push("this"); // push a string on the stack at the end
 // var a = myArray.pop(); // pops the last element from the stack
 // console.log(a); // prints "!"
 // console.log(myArray); // prints ["this", "is", "neat"]
 
-
-
-
-
-function isLegal() {
+const isLegal = (originDisc, destination) => {
   // Your code here
   //a disc can move across any stack
   //large disc cannot be placed on top of a smaller disc.
   //I will need to use an if/else statement to determine if the move is legal or not legal.
-  //I will need to use a for loop (forEach) to determine the items in each array.
   // if disc is 1 or 2 or 3 or 4 and stack contains no discs, then move is legal.
-  //if disc is 1 and stack contains disc 2 or 3 or 4, then move is legal.
-  // if disc is 2 and stack contains disc 1, then move is not legal. If it contains disc 3 or disc 4, then it's legal.
-  // if disc is 3 and stack contains disc 1 or disc 2, then move is not legal. If it contains disc 4, then it's legal.
-  // if disc is 4 and stack contains 1 or 2 or 3, then move is not legal.
   //Legal move – move one piece at a time, piece can go only on the bigger piece or on the empty spot
-
+  //disc from origin bigger than the disc on the destination
+  const destinationDisc = destination[destination.length - 1];
+  if (destination.length === 0) {
+    return true;
+  } else if (originDisc < destinationDisc) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
-function checkForWin() {
+const checkForWin = () => {
   // Your code here
   //a win is true when in the stacks variable in object c, 4321 is in the array.
   //I will use an if/else statement to check for true/false and probably need a for loop (forEach) to check for the items in teh array.
-  let win = stacks.c.length;
-  if (win === 4) {
-    return movePiece
+  if (stacks.c.length === 4) {
+    return console.log("You win");
+  } else if (stacks.b.length === 4) {
+    return console.log("You win");
   } else {
-    //don't allow item to move
+    return false;
   }
 }
-
-
-function towersOfHanoi(startStack, endStack) {
+const towersOfHanoi = (startStack, endStack) => {
   // Your code here
   //it's a loop that calls movePiece for each element in the stack to the endStack
-
+  const origin = stacks[startStack];
+  const destination = stacks[endStack];
+  if (!origin.length) {
+    return console.log("There are no discs on the stack");
+  }
+  const originDisc = origin[origin.length - 1];
+  if (!isLegal(originDisc, destination)) {
+    return console.log("This is a not a legal move");
+  }
+  movePiece(origin, originDisc, destination);
+  checkForWin();
+  resetGame();
 }
-
-function getPrompt() {
+//reset the game
+const resetGame = () => {
+  if (stacks.c.length == 4) {
+    stacks.c.length = 0;
+    stacks.a = [4, 3, 2, 1]
+  }
+}
+const getPrompt = () => {
   printStacks();
   rl.question('start stack: ', (startStack) => {
     rl.question('end stack: ', (endStack) => {
