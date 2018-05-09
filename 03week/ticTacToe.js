cca371b6b6eb2d9c15c488e6eade81c2a3df7449
Lesley Sablatura <lesleysab210@gmail.com>
2018-05-08 19:15:36 -0500

    latest updates

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -28,28 +28,34 @@ function printBoard() {
 
 
 function horizontalWin() {
-<<<<<<< Updated upstream
-  board.forEach(function(item) {
-    console.log()
-  });
+  if (board.includes("X", [0, 0], [0, 1], [0, 2])) {
+    return true;
+  } else if (board.includes("X", [1, 0], [1, 1], [1, 2])) {
+    return true;
+  } else if (board.includes("X", [2, 0], [2, 1], [2, 2])) {
+    return true;
+  }
   // Your code here
   //check each array for  3 X or O
   //check 1st array 0 and all elements for x, y. and all positions are returned, then x or y wins
   //check 2nd array 1 and all elements for x, y. and all positions are returned, then x or y wins
   //check 3rd array 2 and all elements for x, y. and all positions are returned, then x or y wins
 
-=======
+
   // winner combos is array 0 in 0 1 2 || array 1 in 0, 1, 2 || in array 2 in 0, 1, 2
   // console.log(board[0][0])
->>>>>>> Stashed changes
+
 }
 
 function verticalWin() {
+  if (board.includes("X", [0, 0], [1, 0], [2, 0])) {
+    return true;
+  } else if (board.includes("X", [0, 1], [1, 1], [2, 1])) {
+    return true;
+  } else if (board.includes("X", [0, 2], [1, 2], [2, 2])) {
+    return true;
+  }
 
-<<<<<<< Updated upstream
-  board.forEach(function(item) {
-    console.log()
-  });
 
   // Your code here
   //check each element 0, 1, 2 in each array for 3 x or 3 O
@@ -65,16 +71,19 @@ function verticalWin() {
   //check element 2 in array 1 for x,y
   //check element 2 in array 2 for xy
   //then x,y wins
-=======
+
   // winner combos in array 0 index 0 + in array 1 index 0 + array 2 index 0 || array 0 index 1 + array 1  index 1 + array 2 index 1 || array 0 index 2 + array 1 index 2 + array 2 index 2
->>>>>>> Stashed changes
+
 }
 
 function diagonalWin() {
+  if (board.includes("X", [0, 0], [1, 1], [2, 2])) {
+    return true;
+  } else if (board.includes("X", [2, 0], [1, 1], [0, 2])) {
+    return true;
+  }
 
-  board.forEach(function(item) {
-    console.log()
-  });
+
   // Your code here
   //check element 0 in array 0
   //check element 1 in array 1,
@@ -83,28 +92,44 @@ function diagonalWin() {
 }
 
 function checkForWin() {
-  // Your code here
+  if (horizontalWin(), true) {
+    return true;
+    console.log("You Win");
+  } else if (verticalWin(), true) {
+    return true;
+    console.log("You Win");
+  } else if (diagonalWin(), true) {
+    return true;
+    console.log("You Win");
+  }
 }
 
+
+
 function ticTacToe(row, column) {
-  // Your code here
-<<<<<<< Updated upstream
-  //player 1 places X on board
-  //player 2 places O on board, but not where there is an X or an O
-  //player 1 places X on board, but not where there is an X or O
-  //check to see if a value exists in the cell, if there is not a value, then an x or o can be placed.
-  //if an x played last, then play an o.
-  //   if (playerTurn = 'X' &&
-  //     index === "") {
-  //     return "X"
-  //   } else {
-  //     return "O"
-  //   }
-=======
-  //is space empty? if yes, place x or o.
-  // board[0][0] = 'X'
->>>>>>> Stashed changes
+
+  if (!playerTurn) {
+    let playerTurn = "O"
+    // Your code here
+
+    //player 1 places X on board
+    //player 2 places O on board, but not where there is an X or an O
+    //player 1 places X on board, but not where there is an X or O
+    //check to see if a value exists in the cell, if there is not a value, then an x or o can be placed.
+    //if an x played last, then play an o.
+    //   if (playerTurn = 'X' &&
+    //     index === "") {
+    //     return "X"
+    //   } else {
+    //     return "O"
+    //   }
+
+    //is space empty? if yes, place x or o.
+    // board[0][0] = 'X'
+  }
+  checkForWin()
 }
+
 
 function getPrompt() {
   printBoard();
